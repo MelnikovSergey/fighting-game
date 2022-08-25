424a920f4766bfc143b0b962a8fb0d0f5128b090
unknown <sergeyandmelnikov@gmail.com>
2022-08-25 23:13:48 +0300

    added new screen and entry point

--- crack-em-done/assets/js/app/controls.js
+++ crack-em-done/assets/js/app/controls.js
@@ -1,36 +1,10 @@
-/*
-
-Key code list:
-- - -
-
-Fighter 1
-- -
-a		65
-w 		87
-d		68
-s		83
-l		76
-k		75
-j		74
-
-- - - - - - - - - -
-
-Fighter 2
-- -
-arrow left	37
-arrow up	38
-arrow right	39
-arrow down	40
-m		77
-n		78
-b		66
-
-*/
-
 window.onkeydown = keyDown;
 window.onkeyup = keyUp;
 
 var actionGame = false;
+
+const fighter1 = document.querySelector('.player_1');
+const fighter2 = document.querySelector('.player_2');
 
 function keyDown(e) {
 
@@ -38,72 +12,72 @@ function keyDown(e) {
     var key = event.keyCode;
     // console.log(key);
 
-    if (key == 83 && !actionGame) {
+    // if (key == 83 && !actionGame) {
         // GameInstance.score.reset();
         // GameInstance.credit.reset();
-	// startGame();
-	// .. 
-	console.log('Game started!');
-    }
+        // startGame();
+        // .. 
+	// console.log('Game started!');
+    // }
 
     // Fighter 1
     if (key == 65) {
 	// ...
-        fighter1.runFramesArray("walkFighter1");
+        // fighter1.runFramesArray("walkFighter1");
     }
     if (key == 87) {
 	// ...
-        fighter1.runFramesArray("jumpFighter1");
+        // fighter1.runFramesArray("jumpFighter1");
     }
     if (key == 83) {
 	// ...
-        fighter1.runFramesArray("downFighter1");
+        // fighter1.runFramesArray("downFighter1");
     }
     if (key == 68) {
 	// ...
-        fighter1.runFramesArray("walkFighter1");
+        // fighter1.runFramesArray("walkFighter1");
     }
     if (key == 76) {
 	// ...
-        fighter1.runFramesArray("punchFighter1");
+        // fighter1.runFramesArray("punchFighter1");
     }
     if (key == 75) {
 	// ...
-        fighter1.runFramesArray("kickFighter1");
+        // fighter1.runFramesArray("kickFighter1");
     }
     if (key == 74) {
 	// ...
-        fighter1.runFramesArray("blockFighter1");
+        // fighter1.runFramesArray("blockFighter1");
     }
 
     // Fighter 2
     if (key == 37) {
 	// ...
-        fighter2.runFramesArray("walkFighter2");
+        // fighter2.runFramesArray("walkFighter2");
     }
     if (key == 38) {
 	// ...
-        fighter2.runFramesArray("jumpFighter2");
+        // fighter2.runFramesArray("jumpFighter2");
     }
     if (key == 40) {
 	// ...
-        fighter2.runFramesArray("downFighter2");
+        // fighter2.runFramesArray("downFighter2");
     }
     if (key == 39) {
 	// ...
-        fighter2.runFramesArray("walkFighter2");
+        // fighter2.runFramesArray("walkFighter2");
     }
     if (key == 77) {
 	// ...
-        fighter2.runFramesArray("punchFighter2");
+        // fighter2.runFramesArray("punchFighter2");
     }
     if (key == 78) {
 	// ...
-        fighter2.runFramesArray("kickFighter2");
+        // fighter2.runFramesArray("kickFighter2");
     }
     if (key == 66) {
 	// ...
-        fighter2.runFramesArray("blockFighter2");
+        // fighter2.runFramesArray("blockFighter2");
     }
 
 }
@@ -113,61 +87,61 @@ function keyUp(e) {
     // Fighter 1
     if (e.keyCode == 65) {
 	// ...
-        fighter1.runFramesArray("standFighter1");
+        // fighter1.runFramesArray("standFighter1");
     }
     if (e.keyCode == 87) {
 	// ...
-        fighter1.runFramesArray("standFighter1");
+        // fighter1.runFramesArray("standFighter1");
     }
     if (e.keyCode == 68) {
 	// ...
-        fighter1.runFramesArray("standFighter1");
+        // fighter1.runFramesArray("standFighter1");
     }
     if (e.keyCode == 83) {
 	// ...
-        fighter1.runFramesArray("standFighter1");
+        // fighter1.runFramesArray("standFighter1");
     }
     if (e.keyCode == 76) {
 	// ...
-        fighter1.runFramesArray("standFighter1");
+        // fighter1.runFramesArray("standFighter1");
     }
     if (e.keyCode == 75) {
 	// ...
-        fighter1.runFramesArray("standFighter1");
+        // fighter1.runFramesArray("standFighter1");
     }
     if (e.keyCode == 74) {
 	// ...
-        fighter1.runFramesArray("standFighter1");
+        // fighter1.runFramesArray("standFighter1");
     }
 
     // Fighter 2
     if (e.keyCode == 37) {
 	// ...
-        fighter2.runFramesArray("standFighter2");
+        // fighter2.runFramesArray("standFighter2");
     }
     if (e.keyCode == 38) {
 	// ...
-        fighter2.runFramesArray("standFighter2");
+        // fighter2.runFramesArray("standFighter2");
     }
     if (e.keyCode == 40) {
 	// ...
-        fighter2.runFramesArray("standFighter2");
+        // fighter2.runFramesArray("standFighter2");
     }
     if (e.keyCode == 39) {
 	// ...
-        fighter2.runFramesArray("standFighter2");
+        // fighter2.runFramesArray("standFighter2");
     }
     if (e.keyCode == 77) {
 	// ...
-        fighter2.runFramesArray("standFighter2");
+        // fighter2.runFramesArray("standFighter2");
     }
     if (e.keyCode == 78) {
 	// ...
-        fighter2.runFramesArray("standFighter2");
+        // fighter2.runFramesArray("standFighter2");
     }
     if (e.keyCode == 66) {
 	// ...
-        fighter2.runFramesArray("standFighter2");
+        // fighter2.runFramesArray("standFighter2");
     }
 
 }
